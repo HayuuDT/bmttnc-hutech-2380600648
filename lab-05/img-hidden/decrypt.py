4f392d6210741b2b33aaa17ab6435a57393716ea
HayuuDT <hieuh894@gmail.com>
2026-06-15 08:50:42 +0700

    lab-02

--- lab-05/img-hidden/decrypt.py
+++ lab-05/img-hidden/decrypt.py
@@ -6,18 +6,23 @@ def decode_image(encoded_image_path):
     width, height = img.size
     binary_message = ""
     
+    # Bước 1: Trích xuất tất cả các bit LSB từ các kênh màu của điểm ảnh
     for row in range(height):
         for col in range(width):
             pixel = img.getpixel((col, row))
             
             for color_channel in range(3):
                 binary_message += format(pixel[color_channel], '08b')[-1]
-                
+
+    # Bước 2: Dịch chuỗi bit thành ký tự và kiểm tra chuỗi kết thúc của encrypt.py
     message = ""
     for i in range(0, len(binary_message), 8):
-        char = chr(int(binary_message[i:i+8], 2))
-        if char == '\xfe':  # Kết thúc thông điệp khi gặp dấu mốc dừng (tương ứng với chuỗi 11111110)
+        # SỬA TẠI ĐÂY: Kiểm tra nếu gặp 16 bit đánh dấu kết thúc thì dừng lại ngay
+        if binary_message[i:i+16] == '1111111111111110':
             break
+            
+        # Nếu chưa gặp ký hiệu kết thúc, tiến hành dịch 8 bit thành 1 ký tự chữ
+        char = chr(int(binary_message[i:i+8], 2))
         message += char
         
     return message
